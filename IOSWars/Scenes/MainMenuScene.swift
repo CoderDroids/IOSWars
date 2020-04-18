5b62807b20419423a4f43832f815bda6b4663f42
Younggi Kim <younggi.kim@puzzlecats.com>
2020-04-18 14:05:43 -0400

    renamed a game title

--- IOSWars/Scenes/MainMenuScene.swift
+++ IOSWars/Scenes/MainMenuScene.swift
@@ -10,7 +10,7 @@ import SpriteKit
 
 
 class MainMenuScene: SKScene {
-    var continueButton : SKSpriteNode!
+    //var continueButton : SKSpriteNode!
     var newGameButton : SKSpriteNode!
     var optionButton : SKSpriteNode!
     var creditButton : SKSpriteNode!
@@ -22,7 +22,7 @@ class MainMenuScene: SKScene {
         var size = self.size
 
         
-        continueButton = self.childNode( withName : "ContinueButton" ) as! SKSpriteNode
+        //continueButton = self.childNode( withName : "ContinueButton" ) as! SKSpriteNode
         newGameButton = self.childNode( withName : "NewGameButton" ) as! SKSpriteNode
         optionButton = self.childNode( withName : "OptionButton" ) as! SKSpriteNode
         creditButton = self.childNode( withName : "CreditButton" ) as! SKSpriteNode
@@ -41,10 +41,11 @@ class MainMenuScene: SKScene {
         if let location = touch?.location( in: self )
         {
             var sceneName : String?
-            if continueButton.contains(location) {
-                Audio.instance.playEffect(name: "click")
-                sceneName = "GameScene"
-            } else if newGameButton.contains(location) {
+//            if continueButton.contains(location) {
+//                Audio.instance.playEffect(name: "click")
+//                sceneName = "GameScene"
+//            } else
+            if newGameButton.contains(location) {
                 Audio.instance.playEffect(name: "click")
                 sceneName = "GameScene"
             } else if optionButton.contains(location) {
